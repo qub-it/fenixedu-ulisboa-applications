663443a34d97e181aaecbc0efa54a25dd6f70abb
Shezad Anavarali <shezad.anavarali@qub-it.com>
2023-07-06 17:34:46 +0100

    Refactor: Make compatible with latest version of academic-core

--- src/main/java/org/fenixedu/ulisboa/applications/services/teacher/calendar/TeacherLessonCalendarService.java
+++ src/main/java/org/fenixedu/ulisboa/applications/services/teacher/calendar/TeacherLessonCalendarService.java
@@ -1,10 +1,18 @@
 package org.fenixedu.ulisboa.applications.services.teacher.calendar;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.fenixedu.academic.domain.ExecutionInterval;
+import org.fenixedu.academic.domain.Lesson;
 import org.fenixedu.academic.domain.Teacher;
+import org.fenixedu.academic.domain.util.icalendar.ClassEventBean;
+import org.fenixedu.academic.domain.util.icalendar.EventBean;
+import org.joda.time.DateTime;
+import org.joda.time.YearMonthDay;
 
 import com.google.common.collect.Sets;
 
@@ -31,8 +39,18 @@ public class TeacherLessonCalendarService {
         return this.teacher.getProfessorships(this.executionSemester).stream()
                 .flatMap(p -> p.getAssociatedShiftProfessorshipSet().stream()).map(sp -> sp.getShift())
                 .flatMap(s -> s.getAssociatedLessonsSet().stream())
-                .flatMap(l -> l.getAllLessonsEvents().stream().map(e -> new TeacherLessonCalendarReport(l, e)))
+                .flatMap(l -> getAllLessonsEvents(l).stream().map(e -> new TeacherLessonCalendarReport(l, e)))
                 .sorted(TeacherLessonCalendarReport.COMPARATOR_BY_BEGIN).collect(Collectors.toList());
+    }
+
+    private static List<EventBean> getAllLessonsEvents(Lesson lesson) {
+        List<EventBean> result = new ArrayList<>();
+        for (YearMonthDay aDay : lesson.getAllLessonDates()) {
+            DateTime beginDate = aDay.toLocalDate().toDateTime(lesson.getBeginHourMinuteSecond().toLocalTime());
+            DateTime endDate = aDay.toLocalDate().toDateTime(lesson.getEndHourMinuteSecond().toLocalTime());
+            result.add(new ClassEventBean(beginDate, endDate, false, Set.of(), null, null, lesson.getShift()));
+        }
+        return result;
     }
 
 }
